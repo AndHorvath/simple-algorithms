df88b5575d557ea9a8bcd897112ae5b583c184ea
András Horváth <horvath.andras@prevaudit.hu>
2023-08-02 22:45:10 +0200

    addition done

--- Addition/Program.cs
+++ Addition/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Addition
+﻿using System.Numerics;
+
+namespace Addition
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int integer;
+            int sum = 0;
+
+            Console.WriteLine("Enter an integer, please. If you want to quit, enter zero.");
+            do
+            {
+                while (!int.TryParse(Console.ReadLine(), out integer))
+                {
+                    Console.WriteLine("Incorrect number entry.");
+                }
+                sum += integer;
+                Console.WriteLine($"Sum of your entries: {sum}");
+            } while (integer != 0);
         }
     }
 }
