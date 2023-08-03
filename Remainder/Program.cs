6dd20bad838ccffd8ab89262d87b523f75006def
András Horváth <horvath.andras@prevaudit.hu>
2023-08-03 19:00:09 +0200

    remainder done

---
+++ Remainder/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Remainder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dividend;
+            int divisor;
+            bool isIncorrectDivisor;
+            string incorrectFormatMessage = "Incorrect number format.";
+            string divisionByZeroMessage = "No division by zero.";
+            
+            Console.WriteLine("Enter dividend.");
+            while (!int.TryParse(Console.ReadLine(), out dividend))
+            {
+                Console.WriteLine(incorrectFormatMessage);
+            }
+            Console.WriteLine("Enter divisor.");
+            while ((isIncorrectDivisor = !int.TryParse(Console.ReadLine(), out divisor)) || divisor == 0)
+            {
+                if (isIncorrectDivisor)
+                {
+                    Console.WriteLine(incorrectFormatMessage);
+                }
+                else
+                {
+                    Console.WriteLine(divisionByZeroMessage);
+                }
+            }
+            Console.WriteLine($"The division's remainder is {dividend % divisor}");
+        }
+    }
+}
