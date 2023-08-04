131ca9f12bb0e0d16923c00472d88b95019cc34c
András Horváth <horvath.andras@prevaudit.hu>
2023-08-05 00:35:58 +0200

    censor done

---
+++ Censor/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Censor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] vowelsLower = { 'a', 'e', 'i', 'o', 'u' };
+            char[] vowelsUpper = Array.ConvertAll(vowelsLower, char.ToUpper);
+            IEnumerable<char> vowels = vowelsLower.Union(vowelsUpper);
+
+            Console.WriteLine("Enter a text, please.");
+            string text = Console.ReadLine();
+
+            foreach (char vowel in vowels)
+            {
+                text = text.Replace(vowel, '*');
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
