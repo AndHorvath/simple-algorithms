67fddd752d01354413f77c567eb2fec2c8f570b1
András Horváth <horvath.andras@prevaudit.hu>
2023-08-05 22:27:07 +0200

    simplifier done

---
+++ Simplifier/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Simplifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any text, please.");
+
+            string text = Console.ReadLine();
+            string simplifiedText = string.Empty;
+
+            foreach (char character in text.ToLower())
+            {
+                if (simplifiedText.Length == 0 || character != simplifiedText[^1])
+                {
+                    simplifiedText += character;
+                }
+            }
+            Console.WriteLine(simplifiedText);
+        }
+    }
+}
