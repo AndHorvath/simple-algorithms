0ed65b5f78f1e8f5faaf31049c9aff55d72579f4
András Horváth <horvath.andras@prevaudit.hu>
2023-08-05 04:05:01 +0200

    memorymachine done

---
+++ MemoryMachine/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MemoryMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = string.Empty;
+            string newText;
+
+            Console.WriteLine(
+                "Enter a text, please.\n" +
+                "If you want to finish, just press Enter without entering anything else.");
+
+            while (!string.IsNullOrEmpty(newText = Console.ReadLine()))
+            {
+                text += newText;
+
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                Console.WriteLine(new string(' ', newText.Length));
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
